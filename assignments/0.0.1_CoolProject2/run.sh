2a719e553e4347018e2a81fc46c4902deb65d2da
KenwoodFox <kenwood364@gmail.com>
2022-04-28 12:07:59 -0400

    experiment with matrixes

---
+++ assignments/0.0.1_CoolProject2/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PROJ_NAME="hello"
+
+# Written by Joe
+# If you do not have bash (or zsh or, something)
+# You can invoke cmake as ususal per your system
+# See the readme at the root of this repo.
+
+cmake . -B_build
+cd _build && make && ./$PROJ_NAME # Expects make

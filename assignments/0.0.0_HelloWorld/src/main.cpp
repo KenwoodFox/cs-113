4eedb350a83ca8145c3bb42d82775808b3752a99
KenwoodFox <kenwood364@gmail.com>
2022-07-27 14:07:40 -0400

    Even cooler

--- assignments/0.0.0_HelloWorld/src/main.cpp
+++ assignments/0.0.0_HelloWorld/src/main.cpp
@@ -2,10 +2,18 @@
  * 2022
  */
 
+#ifndef MOTD
+#define MOTD "?"
+#endif
+
+#include <iostream>
+
 #include "printhello.h"
 
 int main()
 {
     printHello();
+
+    std::cout << MOTD; // Print build info
     return 0;
 }

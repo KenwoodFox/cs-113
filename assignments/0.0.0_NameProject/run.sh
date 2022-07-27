0bb286aac7c62435cef09ba9345d8b7e55b11ba0
KenwoodFox <kenwood364@gmail.com>
2022-07-27 13:59:30 -0400

    This is very cool but a teeny impracical, but still cool

--- assignments/0.0.0_NameProject/run.sh
+++ assignments/0.0.0_NameProject/run.sh
@@ -7,10 +7,24 @@
 
 cmake . -B_build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
+projName=$(pwd | awk -F / '{print $NF}') # Get project name
+
 if [[ $* == *--test* ]]
 then
     cd _build && make
-    python ../test.py
+    python ../test.py | sed 's/\x1B\[[0-9;]\{1,\}[A-Za-z]//g' &> results.txt # Pipe the test results to a file (Also strips the color codes with sed)
+
+    # Build the docs
+    cp ../*.tex .
+	cp ../*.sty .
+	find . -name '*.tex' -exec latexmk --shell-escape -pdf {} \;
+
+    # Stage and zip
+    mkdir -p bin
+    cp *_binary bin/
+
+    tar -zcvf $projName.tar.gz bin/ Writeup.pdf
 else
     cd _build && make && ./*_binary # Expects make
 fi
+

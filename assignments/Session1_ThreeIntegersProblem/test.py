ac94355bfdcade1ad89a9dede2b22153f0aeac8e
KenwoodFox <kenwood364@gmail.com>
2022-09-06 14:27:05 -0400

    Fixup Three Ints Problem

--- assignments/Session1_ThreeIntegersProblem/test.py
+++ assignments/Session1_ThreeIntegersProblem/test.py
@@ -1,6 +1,7 @@
 # Written by joe
 
 import os, sys
+from socket import timeout
 import fnmatch
 import pexpect
 
@@ -11,8 +12,12 @@ for file in os.listdir("."):
         pro.logfile = sys.stdout.buffer
 
         try:
-            pro.sendline("0")
-            pro.expect("1*", timeout=1)
+            pro.sendline("4")
+            pro.sendline("2")
+            pro.sendline("7")
+
+            pro.expect("Sum of three numbers is 13", timeout=1)
+            pro.expect("Mean of three numbers is 4*", timeout=1)
         except:
             print("FAIL")
             sys.exit(1)

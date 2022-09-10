e4535234c3b8f6ba4c02ffc0e136837850267d1a
KenwoodFox <kenwood364@gmail.com>
2022-09-09 23:51:02 -0400

    Add basics and simple 3d models

---
+++ assignments/Extra1_AnimatedHelloWorld/src/main.cpp
@@ -0,0 +1,18 @@
+/* Joe
+ * 2022
+ */
+
+#ifndef MOTD
+#define MOTD ""
+#endif
+
+#include <stdio.h>
+#include <stdint.h>
+
+int main()
+{
+    // My code here..
+
+    printf(MOTD); // Print build info
+    return 0;
+}

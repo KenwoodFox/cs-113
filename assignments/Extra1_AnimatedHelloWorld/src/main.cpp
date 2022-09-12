59dea0700d1a838565b0d999e41c96057baca61d
KenwoodFox <kenwood364@gmail.com>
2022-09-12 12:29:17 -0400

    Implement some glm stuff

--- assignments/Extra1_AnimatedHelloWorld/src/main.cpp
+++ assignments/Extra1_AnimatedHelloWorld/src/main.cpp
@@ -8,10 +8,21 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <iostream>
+
+// # Includes for our own headers
+#include "loader.h"
+
+// Graphics
+std::vector<glm::vec3> cur_vertices; // Every vertice were drawing on the screen now
+std::vector<glm::vec2> cur_uvs;
+std::vector<glm::vec3> cur_normals;
 
 int main()
 {
-    // My code here..
+    std::cout << loadOBJ("../3d/Cube.obj", cur_vertices, cur_uvs, cur_normals);
+
+    // std::cout << glm::to_string(cur_vertices) << std::endl;
 
     printf(MOTD); // Print build info
     return 0;

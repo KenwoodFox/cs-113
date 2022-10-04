94bbd0cb89711b259edf917a77624c7ab0c9f17d
KenwoodFox <kenwood364@gmail.com>
2022-10-04 11:56:52 -0400

    Complete teamwork team 4

---
+++ assignments/Team4_WageCalculator/src/main.cpp
@@ -0,0 +1,35 @@
+/* Team 4 <3
+ * 2022
+ */
+
+#ifndef MOTD
+#define MOTD ""
+#endif
+
+#include <stdio.h>
+#include <stdint.h>
+
+static const int REG_PAY_PER_HR = 20;
+static const int MAX_HOURS = 40;
+static const double OVERTIME_MOD = 1.5;
+
+int main()
+{
+    int hours_worked = 0;
+
+    printf("Enter your hours worked this week: ");
+
+    scanf("%d", &hours_worked);
+
+    int regular_hours, overtime_hours;
+    regular_hours = hours_worked > MAX_HOURS ? 40 : hours_worked;             // Truncate
+    overtime_hours = hours_worked > MAX_HOURS ? hours_worked - MAX_HOURS : 0; // Truncate
+
+    printf("You worked %d hours, %d of witch are paid at your regular rate %d/hr, and the other %d are %.1lf times that.\n\n", hours_worked, regular_hours, REG_PAY_PER_HR, overtime_hours, OVERTIME_MOD);
+    int reg_pay = regular_hours * REG_PAY_PER_HR;
+    int overtm_pay = overtime_hours * (OVERTIME_MOD * regular_hours);
+    printf("Normal  \t%d\t%d\nOvertime\t%d\t%d\n\nTotal   \t%d", regular_hours, reg_pay, overtime_hours, overtm_pay, reg_pay + overtm_pay);
+
+    printf(MOTD); // Print build info
+    return 0;
+}

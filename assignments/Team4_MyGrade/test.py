27e3b62417c47c52fd4ac8d1e46d965b722ecf99
KenwoodFox <kenwood364@gmail.com>
2022-10-04 12:04:34 -0400

    Add test for last

--- assignments/Team4_MyGrade/test.py
+++ assignments/Team4_MyGrade/test.py
@@ -11,8 +11,8 @@ for file in os.listdir("."):
         pro.logfile = sys.stdout.buffer
 
         try:
-            pro.sendline("0")
-            pro.expect("1*", timeout=1)
+            pro.sendline("50")
+            pro.expect("F*", timeout=1)
         except:
             print("FAIL")
             sys.exit(1)

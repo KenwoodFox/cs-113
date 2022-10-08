65f47a8085bf681898eb862742c3d4fa08bd8699
KenwoodFox <kenwood364@gmail.com>
2022-10-07 22:16:30 -0400

    Meh make this work

--- assignments/Session3_Statistician/test.py
+++ assignments/Session3_Statistician/test.py
@@ -11,8 +11,28 @@ for file in os.listdir("."):
         pro.logfile = sys.stdout.buffer
 
         try:
+            pro.sendline("10")
+            pro.sendline("2")
+            pro.sendline("25")
+            pro.sendline("-3")
+            pro.sendline("22")
+            pro.sendline("-14")
+            pro.sendline("-14")
             pro.sendline("0")
-            pro.expect("1*", timeout=1)
+            pro.sendline("11")
+            pro.sendline("16")
+
+            pro.expect("Sum of positive numbers         86", timeout=1)
+            pro.expect("Number of positive numbers      6", timeout=1)
+            pro.expect("Average of positive numbers     1", timeout=1)
+
+            pro.expect("Sum of negative numbers         -31", timeout=1)
+            pro.expect("Number of negative numbers      4", timeout=1)
+            pro.expect("Average of negative numbers     1", timeout=1)
+
+            pro.expect("Sum of all numbers              10", timeout=1)
+            pro.expect("Number of all numbers           10", timeout=1)
+            pro.expect("Average of all numbers          1*", timeout=1)
         except:
             print("FAIL")
             sys.exit(1)
